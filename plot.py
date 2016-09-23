bad9ea929c0723bf33a86c31c8c9486a4cabad78
Gitawi <Gitawi@jbu.se>
2016-09-23 23:11:40 +0200

    plot funkar nu

--- plot.py
+++ plot.py
@@ -2,26 +2,47 @@ import matplotlib.pyplot as plt
 import numpy as np
 import wave
 import sys
-
-file = "E:\Git\RecordAudio\slask\demo00.wav"
-spf = wave.open(file,'r')
-
-#Extract Raw Audio from Wav File
-signal = spf.readframes(-1)
-signal = np.fromstring(signal, 'Int16')
-fs = spf.getframerate()
-
-#If Stereo
-if spf.getnchannels() == 2:
-    print( 'Just mono files')
-    sys.exit(0)
-
-Time=np.linspace(0, len(signal)/fs, num=len(signal))
-
-plt.figure(1)
-plt.title('Signal Wave...')
-plt.plot(Time, signal)
-plt.show()
+from os  import listdir
+# from scandir
+# scandir
 
 
+Directory = "E:\Git\RecordAudio\slask\\"
+
+for file in listdir(Directory):
+    if file.endswith(".wav")  :
+        file = Directory + file
+        # print(os.path.join(directory, filename))
+ 
+        spf = wave.open(file,'rb')
+
+        #Extract Raw Audio from Wav File
+        signal = spf.readframes(-1)
+        signal = np.fromstring(signal, 'Int16')
+        fs = spf.getframerate()
+
+        #If Stereo
+        if spf.getnchannels() == 2:
+            print( 'Just mono files')
+            sys.exit(0)
+
+        Time=np.linspace(0, len(signal)/fs, num=len(signal))
+
+        plt.figure(1)
+        plt.title('Signal Wave...')
+        plt.plot(Time, signal)
+        # plt.show()
+
+        filen = file.replace(".wav",".png")
+        plt.savefig(filen, bbox_inches='tight')
+        # filen = filen.replace(".pdf",".png")
+        # plt.savefig(filen, bbox_inches='tight')
+
+        plt.close()
+
+
+
+        continue
+    else:
+        continue
 

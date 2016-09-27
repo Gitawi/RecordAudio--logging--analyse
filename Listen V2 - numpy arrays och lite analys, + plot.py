22c10d9ea568e1b85136a5fb6c35dfd4569bf556
Gitawi <Gitawi@jbu.se>
2016-09-27 20:48:09 +0200

    Rensat lite, Listen V2 fubkar bra

--- Listen V2 - numpy arrays och lite analys, + plot.py
+++ Listen V2 - numpy arrays och lite analys, + plot.py
@@ -11,31 +11,6 @@ import wave
 import timeit
 
 
-class RingBuffer():
-    "A 1D ring buffer using numpy arrays"
-    def __init__(self, length):
-        self.data = np.zeros(length, dtype='f')
-        self.index = 0
-
-    def extend(self, x):
-        "adds array x to ring buffer"
-        x_index = (self.index + np.arange(x.size)) % self.data.size
-        self.data[x_index] = x
-        self.index = x_index[-1] + 1
-
-    def get(self):
-        "Returns the first-in-first-out data in the ring buffer"
-        idx = (self.index + np.arange(self.data.size)) %self.data.size
-        return self.data[idx]
-
-def ringbuff_numpy_test():
-    ringlen = 100000
-    ringbuff = RingBuffer(ringlen)
-    for i in range(40):
-        ringbuff.extend(np.zeros(10000, dtype='f')) # write
-        ringbuff.get() #read
-
-
 # Autorecording choices, seconds
 THRESHOLD = 7000
 TimeBeforeStart = 3
@@ -61,41 +36,20 @@ def listen() :
         frames_per_buffer=CHUNK_SIZE)
     
     ListQ  = [[("silent", "Max", "Std", "MeaDivMed", "points", "arg", "Var", "Mean", "Median"),[]]]
-    TimeList = [["ii", "Anatime","Arrtime", "Listtime"]]
+    # TimeList = [["ii", "Anatime","Arrtime", "Listtime"]]
 
+    
+    # Starta loopen med inspelning
     for ii in range(100) :
-        #snd_data = array('h', stream.read(CHUNK_SIZE))
         arr = np.array(array('h', stream.read(CHUNK_SIZE)))
-        repetitions = range(1000)    
         if byteorder == 'big':
             snd_data.byteswap()
             arr.byteswap()
-
-        # Är chunken tyst??
-        # Anatime0 = process_time()
-        # for xx in  range(2000) :
-        # ChunkMaxAmp = AnalyzeChunk(snd_data)
-
-         
-        # Anatime = process_time() - Anatime0
-
         
-
-        # Addera t kön
-        # Arrtime0 = process_time()
-        # for xx in  range(2000)  :
-        # ChunkQ.extend(snd_data)
-        # Arrtime = -(Arrtime0 - process_time())
-        # ListQ.append(ii,("prp",snd_data))
-        # Listtime0 = process_time()
-        # for xx in  range(2000)  :
+        # Initierar med arr dumpar
         ListQ.append([( "", "", "", "", "","points", "", "",""),arr])
-        # Listtime = -(Listtime0 - process_time())
-        y=0
-        # TimeList.append([(ii,Anatime, Arrtime,Listtime)])
       
-    z=32
-
+    # Analysera Listan med ljud, per dum och slå ihop en hel ArrTot
     ArrTot0 = array('h')
     for ii in range(1,101) :
         tup = ListQ[ii][0]
@@ -103,7 +57,6 @@ def listen() :
         ArrMaxAmp = np.amax(np.absolute(arr))
         ArrMaxNdx = np.argmax(np.absolute(arr)) 
         ArrMean   = round(np.mean(np.absolute(arr)) ) 
-        # ArrAverage= np.average(np.absolute(arr))  
         ArrMedian   = np.median(np.absolute(arr))
         ArrMeaDivMed = round(ArrMean/ArrMedian)
         ArrVar   = round(np.var(np.absolute(arr)) )
@@ -117,12 +70,10 @@ def listen() :
         newtup = (silent, ArrMaxAmp, ArrStd, ArrMeaDivMed, "points", ArrMaxNdx, ArrVar, ArrMean, ArrMedian )
 
         ListQ[ii][0] = newtup
-
-
-
-    x=0
     ArrTot = np.array(ArrTot0)
 
+
+    # Prepare for plottin
     ArrMax = [yy[1] for yy in [xx[0] for xx in ListQ]]
     ArrStd = [yy[2] for yy in [xx[0] for xx in ListQ]]
     ArrDiv = [yy[3] for yy in [xx[0] for xx in ListQ]]
@@ -136,18 +87,10 @@ def listen() :
         ArrDiv[ii] = ArrDiv[ii] * 1000
         ArrVar[ii]  = round(ArrVar[ii] /1000)
     
-    data = zip(ArrMax,ArrStd)
-    
     plt.figure(1)
-    plt.title('Audio Silence  ...')
-    # plt.scatter(*zip(*data))
+    plt.title('Audio nyckelvärden  ...')
     plt.xlabel('ArrMax')
     plt.ylabel('ArrStd')
-    # data = ListQ
-    # x_val = [ArrMax for y in data]
-    # y_val = [ArrStd for y in data]
-    t = np.arange(0.,130.,10.,)
-
 
     plt.plot(ArrDiv,'bs')
     plt.plot(ArrStd,'or')
@@ -156,17 +99,13 @@ def listen() :
 
 
     plt.figure(2)
-
+    plt.title('Audio filen  ...')
     plt.plot(ArrTot)
 
-
-    #plt.plot(data,'or')
+    # Visa bådda 
     plt.show()
-    # plt.plot(ListQ[][0],ListQ[][2],ListQ[][3])
-  
-   
-    # plt.show()
 
+    # # Spara som fil
     # filen = file.replace(".wav",".png")
     # plt.savefig(filen, bbox_inches='tight')
     # filen = filen.replace(".pdf",".png")
@@ -174,7 +113,7 @@ def listen() :
 
     plt.close()
 
-    rr = 0
+    rr = 0 # Stanna upp en stund
 
 # ringbuff_numpy_test()
 
